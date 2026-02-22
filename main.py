a65e9c78fb944e0b2b9f26f525c277d6a9966030
kavya-gouda <kavyagoudam21@gmail.com>
2026-02-22 12:55:10 +0530

    test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-head = [3,2,0,-4]
-hashmap = {head[i] : i for i in range(len(head))}
-print(hashmap)
-for i in hashmap:
-    print(i)
-
+ransomNote = "a", magazine = "b"
+for i in ransomNote:
+    if i in magazine:
+        magazine = magazine.replace(i, "", 1)
+    else:
+        print(False)
+        break
